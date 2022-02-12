28bb3dd86421951ac9f4fe173858a9053adbddd1
HRISHIKESH <hrishibhagat08@gmail.com>
2022-02-12 10:06:46 +0530

    6th feb assignment added

--- ASSIGNMENT6FEB/assign6feb.js
+++ ASSIGNMENT6FEB/assign6feb.js
@@ -1,4 +1,4 @@
-// Convert a given array in descending order using sort method====>
+// Convert a given array in descending order using sort method=====>
 
 let fruits = ["Strawberry", "Apple", "Grapes", "Banana", "Kiwi"];
 console.log(fruits);
